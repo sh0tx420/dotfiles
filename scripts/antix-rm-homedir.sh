20f4efc34dcf2828c2ffc6b9e28f1f57be7c1686
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2022-06-01 10:18:12 +0000

    Create antix-rm-homedir.sh

---
+++ scripts/antix-rm-homedir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo rm -rfv .cache/ .dbus/ .desktop-session/ .dillo/ .foxrc/ .fluxbox/ .gconf/ .jwm/ .mcthemes/ .screenlayout/ .xmms/ .newsboat/ .icewm/ .icons/
+sudo rm -rfv Desktop/ Documents/ Downloads/ Music/ Pictures/ Videos/
+sudo rm -fv .conky* .gexec  .gtk* .jwm* .nanorc .Xauthority .Xresources .smb* .fehbg
+echo 'Cleaned home directory'
