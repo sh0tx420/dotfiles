41fa89f9b47a603282288a9b629f4c217b965dcd
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2022-06-04 16:02:40 +0000

    Updated debloat.sh

--- scripts/antix-full-debloater/debloat.sh
+++ scripts/antix-full-debloater/debloat.sh
@@ -10,9 +10,11 @@ done <$FILE_PKGS
 
 # Remove the dependencies from removed packages
 sudo apt autoremove
+echo -e "\e[1;33mPurged all unnecessary packages.\e[0m"
 
 # Remove useless files from home directory
 cd ~/
 sudo rm -rfv .cache/ .dbus/ .desktop-session/ .dillo/ .foxrc/ .fluxbox/ .gconf/ .jwm/ .mcthemes/ .screenlayout/ .xmms/ .newsboat/ .icewm/ .icons/
 sudo rm -rfv Desktop/ Documents/ Downloads/ Music/ Pictures/ Videos/
 sudo rm -fv .conky* .gexec .gtk* .jwm* .nanorc .Xauthority .Xresources .smb* .fehbg
+echo -e "\e[1;33mRemoved unnecessary files from home directory.\e[0m"
