a5f01cd32a3e745f246f9672bef0a0d33e37675e
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2024-08-09 17:10:29 +0300

    Delete cringe stuff

--- scripts/antix-rm-homedir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo rm -rfv .cache/ .dbus/ .desktop-session/ .dillo/ .foxrc/ .fluxbox/ .gconf/ .jwm/ .mcthemes/ .screenlayout/ .xmms/ .newsboat/ .icewm/ .icons/
-sudo rm -rfv Desktop/ Documents/ Downloads/ Music/ Pictures/ Videos/
-sudo rm -fv .conky* .gexec  .gtk* .jwm* .nanorc .Xauthority .Xresources .smb* .fehbg
-echo 'Cleaned home directory. Please remove antix-rm-homedir.sh.'
